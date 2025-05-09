01071e538c06db6a381d50e1dc1da9c852bd60ab
itsbouchra <bouchrasifeddine@gmail.com>
2025-05-09 13:44:02 +0100

    welcomepage et tailwind

---
+++ StockManagerApp/src/navigation/AppNavigator.js
@@ -0,0 +1,30 @@
+// src/navigation/AppNavigator.js
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import WelcomeScreen from '../screens/WelcomeScreen';
+import HomeScreen from '../screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
+
+const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Screen
+          name="Welcome"
+          component={WelcomeScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Dashboard' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigator;
---
+++ StockManagerApp/src/screens/WelcomeScreen.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import { View, Text, StyleSheet, ImageBackground, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+const WelcomeScreen = () => {
+  const navigation = useNavigation();
+
+  return (
+    <ImageBackground
+      source={require('../../src/assets/bg.jpg')}
+      style={styles.background}
+      resizeMode="cover"
+    >
+      <View style={styles.overlay}>
+        <View style={styles.textContainer}>
+          <Text style={styles.title}>Welcome               to the Ultimate Inventory Solution</Text>
+          <Text style={styles.subtitle}>The smart way to manage your stock.</Text>
+        </View>
+
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => navigation.navigate('Home')}
+          >
+            <Text style={styles.buttonText}>Sign in</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={[styles.button, styles.secondaryButton]}>
+            <Text style={styles.buttonText}>Create an account</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </ImageBackground>
+  );
+};
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+  },
+  overlay: {
+    flex: 1,
+    backgroundColor: 'rgba(16, 16, 16, 0.27)', // simulates blur
+    paddingHorizontal: 30,
+    paddingVertical: 60,
+    justifyContent: 'space-between',
+  },
+  textContainer: {
+    marginTop: 60,
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    fontStyle: 'italic',
+    color: '#fff',
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontSize: 14,
+    color: '#ddd',
+    fontStyle: 'italic',
+  },
+  buttonContainer: {
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: '#4c6c43',
+    padding: 15,
+    borderRadius: 25,
+    alignItems: 'center',
+    marginBottom: 15,
+  },
+  secondaryButton: {
+    backgroundColor: '#4c6c43',
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: '600',
+    fontSize: 14,
+
+  },
+});
+
+export default WelcomeScreen;
